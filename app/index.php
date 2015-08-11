85fb7f1fb52961ecc24b41d84ed85da2a22a26a4
Josh Padnick <josh.padnick@gmail.com>
2015-08-10 18:28:36 -0700

    Trivial change to trigger build.

--- app/index.php
+++ app/index.php
@@ -1,4 +1,5 @@
 <?
   date_default_timezone_set('UTC');
-  echo "Today is " . date('l, F jS, Y.') . " Right now it's " .  date('h:i:s A') . " UTC\n";
+  echo "<h2>Hello World!</h2>";
+  echo "<p>Today is " . date('l, F jS, Y.') . " Right now it's " .  date('h:i:s A') . " UTC</p>\n";
 ?>
