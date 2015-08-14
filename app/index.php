074ffea8c0096f47827e8891fd43e7c2962eda7d
Josh Padnick <josh.padnick@gmail.com>
2015-08-14 01:01:41 -0700

    Display EC2 instance id.

--- app/index.php
+++ app/index.php
@@ -1,4 +1,7 @@
 <?
+  EC2_INSTANCE_ID=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+
   date_default_timezone_set('UTC');
+  echo "$EC2_INSTANCE_ID:\n"
   echo "Today is " . date('l, F jS, Y.') . " Right now it's " .  date('h:i:s A') . " UTC\n";
 ?>
