1661d5a48ef3ecefd49fb73de7348c2cffa5b597
Josh Padnick <josh.padnick@gmail.com>
2015-08-14 01:21:49 -0700

    Use AWS SDK for PHP to identify current EC2 instance.

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,13 @@ FROM php:5.6-apache
 # Copy our source code to the container
 ADD app/ /var/www/html/
 
+# Install composer
+WORKDIR /root
+RUN \
+    apt-get update && \
+    apt-get -y install wget && \
+    wget https://github.com/aws/aws-sdk-php/releases/download/2.8.18/aws.phar && \
+    chmod 777 /root/aws.phar
+
 # Listen on port 80
 EXPOSE 80
--- app/index.php
+++ app/index.php
@@ -1,7 +1,12 @@
 <?
-  EC2_INSTANCE_ID=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+  require '/root/aws.phar';
+
+  $ec2_instance = \Aws\Common\InstanceMetadata\InstanceMetadataClient::factory()
+    ->get('dynamic/instance-identity/document')
+    ->send()
+    ->json();
 
   date_default_timezone_set('UTC');
-  echo "<h2>$EC2_INSTANCE_ID:</h2>";
+  echo "<h2>$ec2_instance['privateIp']:</h2>";
   echo "<p>Today is " . date('l, F jS, Y.') . " Right now it's " .  date('h:i:s A') . " UTC</p>\n";
 ?>
